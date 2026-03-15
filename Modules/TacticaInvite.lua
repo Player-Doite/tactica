018945e795e383264070c7dac7a3c07745379df0
Player-Doite <wow-doite@hotmail.se>
2026-03-15 20:48:35 +0100

    added some auto inv words

--- Modules/TacticaInvite.lua
+++ Modules/TacticaInvite.lua
@@ -224,12 +224,12 @@ end
 local PURE_DPS = { hunter=true, mage=true, rogue=true, warlock=true }
 
 local ROLE_KEY = {
-  tank="TANK", tanks="TANK", prot="TANK", protection="TANK", shield="TANK", bear="TANK", furyprot="TANK", ot="TANK", mt="TANK",
-  heal="HEALER", healer="HEALER", heals="HEALER", resto="HEALER", holy="HEALER", disc="HEALER", discipline="HEALER", spriest="DPS",
+  tank="TANK", tanks="TANK", prot="TANK", protection="TANK", shield="TANK", bear="TANK", furyprot="TANK", ot="TANK", mt="TANK", 
+  heal="HEALER", healer="HEALER", heals="HEALER", resto="HEALER", holy="HEALER", disc="HEALER", discipline="HEALER", spriest="DPS", fwar="DPS",
   dps="DPS", dd="DPS", damage="DPS", fury="DPS", arms="DPS", enh="DPS", enhancement="DPS", elemental="DPS", ele="DPS", hunter="DPS", mage="DPS", rogue="DPS", warlock="DPS",
   balance="DPS", boomkin="DPS", moonkin="DPS", shadow="DPS", sp="DPS", cat="DPS", feral="DPS", mm="DPS", marks="DPS", marksmanship="DPS", survival="DPS", bm="DPS", sv="DPS", surv="DPS",
-  combat="DPS", assassin="DPS", assassination="DPS", subtlety="DPS", sub="DPS", daggers="DPS", swords="DPS", rdps="DPS", mdps="DPS", boomi="DPS", boomie="DPS",
-  ["+tank"]="TANK", ["+heal"]="HEALER", ["+heals"] = "HEALER", ["+dps"] = "DPS", rsham="HEALER", enh="DPS"
+  combat="DPS", assassin="DPS", assassination="DPS", subtlety="DPS", sub="DPS", daggers="DPS", swords="DPS", rdps="DPS", mdps="DPS", boomi="DPS", boomie="DPS", rdrood="HEALER",
+  ["+tank"]="TANK", ["+heal"]="HEALER", ["+heals"] = "HEALER", ["+dps"] = "DPS", rsham="HEALER", enh="DPS", hpala="HEALER", restosham="HEALER", restoshaman="HEALER", ["tank+"]="TANK", ["heal+"]="HEALER", ["heals+"] = "HEALER", ["dps+"] = "DPS"
 }
 
 local CLASS_KEY = {
@@ -244,7 +244,7 @@ local SPEC2CLASS = {
   disc="priest", discipline="priest", spriest="priest", rsham="Shaman", sham="Shaman",
   ret="paladin", retribution="paladin", prot="paladin", pal="paladin", pala="paladin",
   enhance="shaman", enhancement="shaman", elemental="shaman", ele="shaman",
-  feral="druid", balance="druid", boomkin="druid", bear="druid", cat="druid",
+  feral="druid", balance="druid", boomkin="druid", bear="druid", cat="druid", drood="druid",
   aff="warlock", affliction="warlock", demo="warlock", demonology="warlock", destro="warlock", destruction="warlock", lock="warlock",
   mm="hunter", marks="hunter", marksmanship="hunter", survival="hunter", bm="hunter", hunt="hunter",
   combat="rogue", assassination="rogue", assassin="rogue", subtlety="rogue", sub="rogue",
